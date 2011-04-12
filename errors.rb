8647d6a702c168aa80053d7f1ff3fe15f51b2431
Mathieu Davy <mathieu.davy@betfair.com>
2011-04-12 17:53:41 +0100

    Adding initial UI

--- errors.rb
+++ errors.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'erb'
 
 CODES = %w{ 100 101 102 122 200 201 202 203 204 205 206 207 226 300 301 302 303 304 305 306 307 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 422 423 424 425 426 444 449 450 499 500 501 502 503 504 505 506 507 509 510 }
 
 get '/' do
-  "Errors is a simple service that allows you to test that your front-end code handles error codes properly"
+  erb :index
 end
 
 get '/k/:code' do
@@ -15,10 +16,17 @@ get '/k/:code' do
     return
   end
   
-  #here should be handlers to take care of each code specifics if required
-  halt params[:code].to_i, params[:code]
+# Todo - Here should be handlers to take care of each code specifics if required
+  @code = params[:code].to_i
+  status @code
+  
+  erb :kode
 end
 
+# Todo - This needs to be differentiated so that when the 404 is *really requested* it
+# => renders the normal view without the "real 404 message"
 not_found do
-  'This is nowhere to be found. By the way, this is because you entered a wrong url! If you really want to test a 404, go to <a href="/404">404</a>'
+  @code = 404
+  
+  erb :kode
 end
