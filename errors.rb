b88b850d6b155431e4ee62097863e0170f293cec
Mathieu Davy <mathieu.davy@betfair.com>
2011-04-12 12:10:33 +0100

    initial commit

---
+++ errors.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+
+get '*favicon.ico' do
+  
+end
+
+get '/' do
+  "Errors is a simple service that allows you to test that your front-end code handles error codes properly"
+end
+
+get '/e/:code' do
+  reg = /^\d+$/
+  
+  if (reg.match(params[:code]))
+    puts 'will halt with '+params[:code]
+    halt params[:code].to_i, params[:code]
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+not_found do
+  'This is nowhere to be found. By the way, this is because you entered a wrong url! If you really want to test a 404, go to <a href="/404">404</a>'
+end
