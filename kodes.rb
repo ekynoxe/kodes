3c182ae6cc07323531b0b73f0892c6f64b0006ab
Mathieu Davy <mathieu.davy@betfair.com>
2011-04-28 15:03:08 +0100

    Adding status codes descriptions and definition through reading a YAML config file

--- kodes.rb
+++ kodes.rb
@@ -1,17 +1,21 @@
+### Dependencies
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'yaml'
 
-CODES = %w{ 100 101 102 122 200 201 202 203 204 205 206 207 226 300 301 302 303 304 305 306 307 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 422 423 424 425 426 444 449 450 499 500 501 502 503 504 505 506 507 509 510 }
+### Configuration
+CODES = YAML.load_file('kodes.yml')
 
+### Controllers
 get '/' do
   erb :index
 end
 
 get '/k/:code' do
   reg = /^\d+$/
-
-  if !reg.match(params[:code]) || !CODES.include?(params[:code])
+  
+  if !reg.match(params[:code]) || !CODES.has_key?(params[:code].to_i)
     raise Sinatra::NotFound
     return
   end
@@ -23,6 +27,7 @@ get '/k/:code' do
   erb :kode
 end
 
+### Error handling
 # Todo - This needs to be differentiated so that when the 404 is *really requested* it
 # => renders the normal view without the "real 404 message"
 not_found do
